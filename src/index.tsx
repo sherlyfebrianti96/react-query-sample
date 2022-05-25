776c299dec86156ae328d71357a0a8b9977f7005
Sherly Febrianti <sherlyfebrianti96@gmail.com>
2022-05-25 13:53:27 +0200

    Install Material UI with Styled Component

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
     <App />
   </React.StrictMode>
 );
