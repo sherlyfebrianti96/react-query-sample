072715450844c81d0b86593f224c9bed8efb5fb5
Sherly Febrianti <sherlyfebrianti96@gmail.com>
2022-05-25 14:32:50 +0200

    Adding the React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>Welcome!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Normal Fetching</Link>
+          </li>
+          <li>
+            <Link to="/users">React Query</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/normal">Normal</Route>
+        <Route path="/react-query">React Query</Route>
+      </Routes>
+    </Router>
   );
 }
 
