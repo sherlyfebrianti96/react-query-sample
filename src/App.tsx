0914f0dde5a473d161dee574c4c43a412f5e0b32
Sherly Febrianti <sherlyfebrianti96@gmail.com>
2022-05-30 10:33:28 +0200

    Installing the React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+export const App = () => {
+  // Create a client
+  const queryClient = new QueryClient();
 
-function App() {
   return (
-    <Router>
-      <h1>Welcome!</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Normal Fetching</Link>
-          </li>
-          <li>
-            <Link to="/users">React Query</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/normal">Normal</Route>
-        <Route path="/react-query">React Query</Route>
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <h1>Welcome!</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Normal Fetching</Link>
+            </li>
+            <li>
+              <Link to="/users">React Query</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/normal">Normal</Route>
+          <Route path="/react-query">React Query</Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
-}
-
-export default App;
+};
